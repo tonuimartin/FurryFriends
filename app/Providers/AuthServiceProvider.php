e5d63e161999a2ae3d08d961badd3e675233fe7a
Tonuimartin <martinkirui57@gmail.com>
2023-06-22 11:43:06 +0300

    remove password tell till fixed

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -32,7 +32,7 @@ class AuthServiceProvider extends ServiceProvider
         return (new MailMessage)
             ->subject('Verify Email Address')
             ->line('Click the button below to verify your email address.')
-            ->lineIf($notifiable->provider,'Please update your password: '.$notifiable->password)
+            // ->lineIf($notifiable->provider,'Please update your password: '.$notifiable->password)
             ->action('Verify Email Address', $url);
     });
 }
