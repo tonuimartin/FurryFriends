1cd9ba3ae48be66d0d8b7c558aee7d9aab9407bb
Laura Wangari <karilaura77@gmail.com>
2023-07-13 04:11:10 +0300

    Homepage Update

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -41,6 +41,7 @@
 
   <!-- ======= Header ======= -->
   <header id="header" class="fixed-top d-flex align-items-center">
+  @include('layouts.message')
     <div class="container d-flex align-items-center">
 
       <h1 class="logo me-auto"><a href="index.html">Furry Friends</a></h1>
@@ -70,13 +71,10 @@
           </li>
           <li><a href="services.html">Services</a></li>
           <li><a href="portfolio.html">Portfolio</a></li>
-          <li><a href="pricing.html">Pricing</a></li>
-          <li><a href="blog.html">Blog</a></li>
-
           <li><a href="contact.html">Contact</a></li>
-          
+
           <li>  @if (Route::has('login'))
-                
+
                     @auth
                         <a href="{{ url('/dashboard') }}" class="getstarted">Dashboard</a>
                     @else
@@ -85,17 +83,22 @@
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="getstarted">Register</a>
                         @endif
+
+                        @if (Route::has('registersource'))
+                            <a href="/registersource" class="getstarted">Register as Source</a>
+                        @endif
                     @endauth
-                
+
             @endif
          </li>
-          
+
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
 
     </div>
   </header><!-- End Header -->
+
 
   <!-- ======= Hero Section ======= -->
   <section id="hero">
@@ -109,9 +112,9 @@
         <div class="carousel-item active" style="background-image: url({{asset('imports/homepage/assets/img/slide/slide-1.jpg')}});">
           <div class="carousel-container">
             <div class="container">
-              <h2 class="animate__animated animate__fadeInDown">Welcome to <span>Furry Friends</span></h2>
-              <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
-              <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
+              <h2 class="animate_animated animate_fadeInDown">Welcome to <span>Furry Friends</span></h2>
+              <p class="animate_animated animate_fadeInUp">Discover Unconditional Love and Joy: Find Your Perfect Companion through Pet Adoption Today!</p>
+              <a href="#about" class="btn-get-started animate_animated animate_fadeInUp scrollto">Read More</a>
             </div>
           </div>
         </div>
@@ -120,9 +123,9 @@
         <div class="carousel-item" style="background-image: url({{asset('imports/homepage/assets/img/slide/slide-2.jpg')}});">
           <div class="carousel-container">
             <div class="container">
-              <h2 class="animate__animated animate__fadeInDown">Lorem Ipsum Dolor</h2>
-              <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
-              <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
+              <h2 class="animate_animated animate_fadeInDown">Rescue And Save A Pet</h2>
+              <p class="animate_animated animate_fadeInUp">Transform Lives By Finding A Furry Friend: All pet services at your fingertips!</p>
+              <a href="#about" class="btn-get-started animate_animated animate_fadeInUp scrollto">Read More</a>
             </div>
           </div>
         </div>
@@ -131,9 +134,9 @@
         <div class="carousel-item" style="background-image: url({{asset('imports/homepage/assets/img/slide/slide-3.jpg')}});">
           <div class="carousel-container">
             <div class="container">
-              <h2 class="animate__animated animate__fadeInDown">Sequi ea ut et est quaerat</h2>
-              <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
-              <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
+              <h2 class="animate_animated animate_fadeInDown">Over 100+ Verified Pet Centres</h2>
+              <p class="animate_animated animate_fadeInUp">"Your Trusted Source for Pet Adoption: Explore a Wide Network of Verified Pet Centers in Kenya on Our Comprehensive Website, Connecting You to Countless Loving Animals Seeking Their Forever Homes!".</p>
+              <a href="#about" class="btn-get-started animate_animated animate_fadeInUp scrollto">Read More</a>
             </div>
           </div>
         </div>
@@ -159,23 +162,22 @@
 
         <div class="row content">
           <div class="col-lg-6">
-            <h2>Eum ipsam laborum deleniti velitena</h2>
-            <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assum perenda sruen jonee trave</h3>
+            <h2>Explore our vast network of likeable pets waiting for you</h2>
+            <h3>We promise to ensure you find a perfect match, and a new home for these amazing animals</h3>
           </div>
           <div class="col-lg-6 pt-4 pt-lg-0">
             <p>
-              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum
+              By choosing to adopt through our website, you are making a compassionate choice to provide a loving home to a deserving animal in need.
+              Our platform ensures a seamless adoption process, connecting you with verified adoption centers and a wide range of pets ready to bring joy into your life.
+              Join our community of caring individuals and experience the rewarding journey of pet adoption today.
             </p>
             <ul>
-              <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequa</li>
-              <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit</li>
-              <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in</li>
+              <li><i class="ri-check-double-line"></i> Wide selection of verified pets from reputable centers</li>
+              <li><i class="ri-check-double-line"></i> Convenient and reliable payment facilities for seamless transactions</li>
+              <li><i class="ri-check-double-line"></i> Fat delivery services - at your doorstep!</li>
             </ul>
             <p class="fst-italic">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua.
+              Don't hesitate to reach out for help, we could connect you to your next soulmate!
             </p>
           </div>
         </div>
@@ -226,43 +228,43 @@
           <div class="col-md-6">
             <div class="icon-box">
               <i class="bi bi-briefcase"></i>
-              <h4><a href="#">Lorem Ipsum</a></h4>
-              <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+              <h4><a href="#">Counsel And Advice</a></h4>
+              <p>We have an outreach desk in case of further inquiries about us or our pets</p>
             </div>
           </div>
           <div class="col-md-6 mt-4 mt-md-0">
             <div class="icon-box">
               <i class="bi bi-card-checklist"></i>
-              <h4><a href="#">Dolor Sitema</a></h4>
-              <p>Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+              <h4><a href="#">Timely Quality Delivery</a></h4>
+              <p>We are always present to get back to you and process your requests</p>
             </div>
           </div>
           <div class="col-md-6 mt-4 mt-md-0">
             <div class="icon-box">
               <i class="bi bi-bar-chart"></i>
-              <h4><a href="#">Sed ut perspiciatis</a></h4>
-              <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+              <h4><a href="#">Incredible Ratings</a></h4>
+              <p>We are proud to be a recognized partner, promising nothing short of the best service</p>
             </div>
           </div>
           <div class="col-md-6 mt-4 mt-md-0">
             <div class="icon-box">
               <i class="bi bi-binoculars"></i>
-              <h4><a href="#">Nemo Enim</a></h4>
-              <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+              <h4><a href="#">Veterinary services</a></h4>
+              <p>We have veterinary services available all around the clock</p>
             </div>
           </div>
           <div class="col-md-6 mt-4 mt-md-0">
             <div class="icon-box">
               <i class="bi bi-brightness-high"></i>
-              <h4><a href="#">Magni Dolore</a></h4>
-              <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
+              <h4><a href="#">Pets in Nature</a></h4>
+              <p>We ensure your pets are kept in good condition with regular access to nature</p>
             </div>
           </div>
           <div class="col-md-6 mt-4 mt-md-0">
             <div class="icon-box">
               <i class="bi bi-calendar4-week"></i>
-              <h4><a href="#">Eiusmod Tempor</a></h4>
-              <p>Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
+              <h4><a href="#">Timely Delivery</a></h4>
+              <p>We simply do not have off days; we deliver once you schedule your delivery with us</p>
             </div>
           </div>
         </div>
@@ -291,8 +293,8 @@
             <div class="portfolio-wrap">
               <img src="imports/homepage/assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
               <div class="portfolio-info">
-                <h4>App 1</h4>
-                <p>App</p>
+                <h4>Bonny</h4>
+                <p>Strict Cat</p>
                 <div class="portfolio-links">
                   <a href="asset('imports/homepage/assets/img/portfolio/portfolio-1.jpg')}}" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 1"><i class="bx bx-plus"></i></a>
                   <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
@@ -305,8 +307,8 @@
             <div class="portfolio-wrap">
               <img src="imports/homepage/assets/img/portfolio/portfolio-2.jpg" class="img-fluid" alt="">
               <div class="portfolio-info">
-                <h4>Web 3</h4>
-                <p>Web</p>
+                <h4>Maya</h4>
+                <p>Shy Pup</p>
                 <div class="portfolio-links">
                   <a href="asset('imports/homepage/assets/img/portfolio/portfolio-2.jpg')}}" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
                   <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
@@ -319,8 +321,8 @@
             <div class="portfolio-wrap">
               <img src="imports/homepage/assets/img/portfolio/portfolio-3.jpg" class="img-fluid" alt="">
               <div class="portfolio-info">
-                <h4>App 2</h4>
-                <p>App</p>
+                <h4>Junior</h4>
+                <p>Happy Cat</p>
                 <div class="portfolio-links">
                   <a href="asset('imports/homepage/assets/img/portfolio/portfolio-3.jpg')}}" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 2"><i class="bx bx-plus"></i></a>
                   <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
@@ -333,8 +335,8 @@
             <div class="portfolio-wrap">
               <img src="imports/homepage/assets/img/portfolio/portfolio-4.jpg" class="img-fluid" alt="">
               <div class="portfolio-info">
-                <h4>Card 2</h4>
-                <p>Card</p>
+                <h4>Pyna</h4>
+                <p>Lovely Dog</p>
                 <div class="portfolio-links">
                   <a href="asset('imports/homepage/assets/img/portfolio/portfolio-4.jpg')}}" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 2"><i class="bx bx-plus"></i></a>
                   <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
@@ -347,8 +349,8 @@
             <div class="portfolio-wrap">
               <img src="imports/homepage/assets/img/portfolio/portfolio-5.jpg" class="img-fluid" alt="">
               <div class="portfolio-info">
-                <h4>Web 2</h4>
-                <p>Web</p>
+                <h4>Guki</h4>
+                <p>Lively Cat</p>
                 <div class="portfolio-links">
                   <a href="asset('imports/homepage/assets/img/portfolio/portfolio-5.jpg')}}" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 2"><i class="bx bx-plus"></i></a>
                   <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
@@ -361,8 +363,8 @@
             <div class="portfolio-wrap">
               <img src="imports/homepage/assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="">
               <div class="portfolio-info">
-                <h4>App 3</h4>
-                <p>App</p>
+                <h4>Fuma</h4>
+                <p>Cute Puppy</p>
                 <div class="portfolio-links">
                   <a href="imports/homepage/assets/img/portfolio/portfolio-6.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 3"><i class="bx bx-plus"></i></a>
                   <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
@@ -375,8 +377,8 @@
             <div class="portfolio-wrap">
               <img src="imports/homepage/assets/img/portfolio/portfolio-7.jpg" class="img-fluid" alt="">
               <div class="portfolio-info">
-                <h4>Card 1</h4>
-                <p>Card</p>
+                <h4>Siti</h4>
+                <p>Sweet kitten</p>
                 <div class="portfolio-links">
                   <a href="imports/homepage/assets/img/portfolio/portfolio-7.jpg data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 1"><i class="bx bx-plus"></i></a>
                   <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
@@ -389,8 +391,8 @@
             <div class="portfolio-wrap">
               <img src="imports/homepage/assets/img/portfolio/portfolio-8.jpg" class="img-fluid" alt="">
               <div class="portfolio-info">
-                <h4>Card 3</h4>
-                <p>Card</p>
+                <h4>Mbiwa</h4>
+                <p>Fastest Dog</p>
                 <div class="portfolio-links">
                 <a href="imports/homepage/assets/img/portfolio/portfolio-8.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 3"><i class="bx bx-plus"></i></a>
                   <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
@@ -403,8 +405,8 @@
             <div class="portfolio-wrap">
               <img src="imports/homepage/assets/img/portfolio/portfolio-9.jpg" class="img-fluid" alt="">
               <div class="portfolio-info">
-                <h4>Web 3</h4>
-                <p>Web</p>
+                <h4>Kiki</h4>
+                <p>Mellow Kitten</p>
                 <div class="portfolio-links">
                   <a href="imports/homepage/assets/img/portfolio/portfolio-9.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
                   <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
